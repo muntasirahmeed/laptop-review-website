538cb4235be762ab67f05a78a15077f8befb4b7c
Muntasir Ahmed <muntasirahmed@Muntasirs-MacBook-Air.local>
2022-04-04 02:35:05 +0600

    review page design done

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ComposedCharts from '../ComposedCharts/ComposedCharts';
 
 const Dashboard = () => {
     return (
       <div>
-        <h1 className="text-3xl">this is Dashboard</h1>
+        <h1 className="text-5xl py-10 text-center">this is Dashboard</h1>
+        <ComposedCharts></ComposedCharts>
       </div>
     );
 };
